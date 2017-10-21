4a4c52441cc9cb154577838fdc7632e97f9b5d57
AdrielFreud <gamerbra39@gmail.com>
2017-10-21 00:05:56 -0200

    Update Consultor_Central.py

--- Consultor_Central.py
+++ Consultor_Central.py
@@ -1,13 +1,10 @@
-#conding: utf-8
-
-#/usr/bin/python
-
 # Desenvolvido por Adriel Freud!
 # Contato: usuariocargo2016@gmail.com 
 # FB: http://www.facebook.com/xrn401
 #   =>DebutySecTeamSecurity<=
-
 #conding: utf-8
+
+#!/bin/usr/python
 import tempfile
 import sys, json
 import requests
@@ -18,7 +15,16 @@ import os
 from time import sleep
 os.system('color a')
 os.system('title Central - Adriel Freud')
+print("""\n
+-----------------------------------------
 
+  #  Desenvolvido por Adriel Freud!
+  #  Contato: usuariocargo2016@gmail.com 
+  #  FB: http://www.facebook.com/xrn401
+  #   =>DebutySecTeamSecurity<=
+
+-----------------------------------------
+\n""")
 temporario = tempfile.gettempdir()
 header = {'user-agent': 'Mozilla/5.0 (X11; Linux i686; rv:43.0) Gecko/10100101 Firefox/43.0 Iceweasel/43.0.4'}
 
@@ -33,23 +39,24 @@ def callmenu():
 		pass
 	main()
 
-
 def CleanTemp():
-	print("\n[!] Limpando arquivos temporario!\n")
-	os.system('del %s /S /Q'%temporario)
-	os.chdir('C:\\Windows\\Temp')
-	os.system('del * /S /Q')
-	os.chdir('C:\\Windows\\Prefetch')
-	os.system('del * /S /Q')
-	os.chdir('C:\\Windows\\SoftwareDistribution\\Download')
-	os.system('del * /S /Q')
-	os.chdir('C:\\Temp')
-	os.system('del * /S /Q')
-	os.system('cd %TEMP%')
-	os.system('del * /S /Q')
-	print("")
-	print("\n[+] Diretorios Limpado com sucesso!")
-
+	try:
+		print("\n[!] Limpando arquivos temporario!\n")
+		os.system('del %s /S /Q'%temporario)
+		os.chdir('C:\\Windows\\Temp')
+		os.system('del * /S /Q')
+		os.chdir('C:\\Windows\\Prefetch')
+		os.system('del * /S /Q')
+		os.chdir('C:\\Windows\\SoftwareDistribution\\Download')
+		os.system('del * /S /Q')
+		os.chdir('C:\\Temp')
+		os.system('del * /S /Q')
+		os.system('cd %TEMP%')
+		os.system('del * /S /Q')
+		print("")
+		print("\n[+] Diretorios Limpado com sucesso!")
+	except:
+		print("[!] Execute como Administrador Para A Remocao!\n\n")
 
 def binchecker():
 	def check():
@@ -85,28 +92,24 @@ def binchecker():
 	binnn = Tk()
 	binnn.title('Bin Checker')
 	binnn['bg'] = 'black'
-
+	#################################
 	labelbin = Label(binnn, text="Insira a Bin", fg="green", bg='black')
 	labelbin.place(x=30, y=100)
-
+	#################################
 	entrybin = Entry(binnn, width=20)
 	entrybin.place(x=75, y=130)
-
-
+	#################################
 	checkbt = Button(binnn, width=20, text="Checkar!", command=check, fg="Green", bg='black')
 	checkbt.place(x=75, y=160)
-
+	#################################
 	exitbin = Button(binnn, width=20, text="Sair!", command=sair, fg="green", bg='black')
 	exitbin.place(x=75, y=190)
-
+	#################################
 	funcMenu = Button(binnn, text="MENU", command=callmenu, fg="Green", bg="black")
 	funcMenu.place(x=75, y=220)
-
-
-
+	#################################
 	binnn.geometry("300x300+200+200")
 	binnn.mainloop()
-
 
 def main():
 	global menu
@@ -114,25 +117,25 @@ def main():
 	menu.title("Menu")
 	info = Label(menu, text="- Central feita por Adriel Freud - ")
 	info.pack(side=TOP)
-
+	#################################
 	con = Button(menu, text="Consultor", command=Consultar, bg="black", fg="green", width=20)
 	con.place(x=75, y=60)
-			
+	#################################
 	bincheck = Button(menu, text="Check Bin", command=binchecker, bg="black", fg="green", width=20)
 	bincheck.place(x=75, y=100)
-
+	#################################
 	clean = Button(menu, text="Limpar Arquivos Temp.", command=CleanTemp, bg="black", fg="green", width=20)
 	clean.place(x=75, y=140)
-
+	#################################
 	exitt = Button(menu, text="Sair!", command=sair, bg="black", fg="green", width=20)
 	exitt.place(x=75, y=180)
-
+	#################################
 	base = Label(menu, text=" ========== Base64 Encode ==========", fg="green", bg="black")
 	base.place(x=20, y=220)
-
+	#################################
 	baseLabel = Label(menu, text="String > ", fg="green", bg="black")
 	baseLabel.place(x=5, y=250)
-
+	#################################
 	baseEntry = Entry(menu, width=24)
 	baseEntry.place(x=75, y=250)
 
@@ -149,18 +152,18 @@ def main():
 		baseEntry = Entry(menu, width=24)
 		baseEntry.place(x=75, y=250)
 
+
 	Buttonbase64 = Button(menu, text="Encodar", command=encodarbase64, fg="green", bg="black", width=20)
 	Buttonbase64.place(x=75, y=290)
-
+	#################################
 	Buttonbase64 = Button(menu, text="CleanString", command=function, fg="green", bg="black", width=20)
 	Buttonbase64.place(x=75, y=320)
-
+	#################################
 	menu['bg'] = 'black'
 	menu.geometry("300x450+200+200")
 	menu.mainloop()
 
 def Consultar():
-
 	def requisitar():
 		cpf = edi2.get()
 		cnpj = edi1.get()
@@ -213,13 +216,13 @@ def Consultar():
 	global window
 	window = Tk()
 	window.title('= Central by AdrielFreud =')
-
+	#################################
 	lb = Label(window, text="Consultor CPF", fg="green", bg='black')
 	lb.place(x=75, y=90)
-
+	#################################
 	l2 = Label(window, text="Consultor CNPJ", fg="green", bg='black')
 	l2.place(x=75, y=150)
-
+	#################################
 	bt1 = Button(window, width=20, text="Consultar", command=requisitar, fg="Green", bg='black')
 	bt1.place(x=75, y=220)
 
@@ -243,17 +246,16 @@ def Consultar():
 	adriellb = Label(window, text="==== Creditos Adriel Freud e Oto Nk ====", fg="green", bg="black")
 	adriellb.pack(side=TOP)
 	adriellb.place(x=35, y=30)
-
+	#################################
 	funcMenu = Button(window, text="MENU", command=callmenu, fg="Green", bg="black")
 	funcMenu.place(x=10, y=150)
-
+	#################################
 	funCLEAR = Button(window, text="CLEAR", command=functionCLEAR, fg="green", bg="black")
 	funCLEAR.place(x=240, y=150)
-
-
+	#################################
 	window['bg'] = 'black'
 	window.geometry("300x300+200+200")
-
+	#################################
 	window.mainloop()
 
 def testar():
@@ -263,12 +265,13 @@ def testar():
 	if (loginn and password == passwd):
 		print("[Logado com Sucesso!]\n")
 		arq = open('conf.deb', 'w')
+		arq.write(password)
 		arq.close()
 		login.destroy()
 		main()
 
 	else:
-		print("[!] login ou Password Invalidos, Tente Novamente!\n")
+		print("[!] Usuario ou Senha Invalidos, Tente Novamente!\n")
 
 def sair():
 	sys.exit()
@@ -279,39 +282,40 @@ def logar():
 	login = Tk()
 	login.title('Login Central - Freud')
 	login['bg'] = "black"
-
+	#################################
 	label1 = Label(login, text="Login: ", bg="black", fg="green")
 	label1.place(x=5, y=30)
-
+	#################################
 	label2 = Label(login, text="Senha: ", bg="black", fg="green")
 	label2.place(x=5, y=60)
-
+	#################################
 	global ed1
 	global ed2
 	ed1 = Entry(login)
 	ed1.place(x=60, y=30)
 	ed2 = Entry(login)
 	ed2.place(x=60, y=60)
-
+	#################################
 	bt1 = Button(login, text="Confirmar", bg="black", fg="green", command=testar, width=20)
 	bt1.place(x=60, y=100)
 	bt2 = Button(login, text="Sair", bg="black", fg="green", command=sair, width=20)
 	bt2.place(x=60, y=130)
-
+	#################################
 	cred = Label(login, text="Facebook: https://www.facebook.com/xrn401", fg="green", bg="black")
 	cred.place(x=10, y=170)
-
-	#label1.grid(row=0, column=0)
-	#label2.grid(row=1, column=0)
-	#ed1.grid(row=0, column=1)
-	#ed2.grid(row=1, column=1)
-	#bt1.grid(row=2, column=1)
-	#bt2.grid(row=3, column=1)
-
+	#################################
 	login.geometry("270x200+100+100")
 	login.mainloop()
 
+##################################################################
+
 if (os.path.exists('conf.deb') == True):
-	main()
+	with open('conf.deb') as conf:
+		linhas = conf.read()
+		if 'adriel' in linhas:
+			main()
+		else:
+			logar()
+		conf.close()
 else:
 	logar()
